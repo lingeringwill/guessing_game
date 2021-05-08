7154d207863021e8340b5195175982e5b8b1b730
joshua <blitzmasterj24@gmail.com>
2021-05-08 11:58:32 -0400

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
 fn main() {
     //sets while loop to be active or inactive
-    let count: bool = false;
+   input();
 }
 
 fn input(){
 
-    println!("please enter in a number");
+    println!("please enter in 5 numbers");
     // stores the users guesses
-    let mut guess_container:[String;5];
-for i in 0..guess_container.len(){ 
-    std::io::stdin().read_line(&mut guess_container)
+    let mut guess_container: Vec<i32> = vec![];
+    let mut buffer = String::new();
+for _i in 0..4{ 
+    std::io::stdin().read_line(&mut buffer)
     .expect("failed to enter value");
+    
+   let buffer: i32 = buffer.parse().expect("Invalid number");
+   guess_container.push(buffer);
+
+
+
    }
    
-   
+   println!("{:?}",guess_container);
     
 }
