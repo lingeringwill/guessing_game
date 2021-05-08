bef600a13595aac6a1afc15a8935b1957a99d2e8
joshua <blitzmasterj24@gmail.com>
2021-05-08 01:39:43 -0400

    first commit

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,11 @@ fn input(){
     println!("please enter in a number");
     // stores the users guesses
     let mut guess_container:[String;5];
-
+for i in 0..guess_container.len(){ 
     std::io::stdin().read_line(&mut guess_container)
     .expect("failed to enter value");
-     //converts every number in the array of strings to a integer value so we can compare them;
-    for i in 0..guess_container.len(){
-        let guess_container: u32 = guess_container.trim().parse().expect("Please type in a number!");
-    }
+   }
+   
+   
     
 }
