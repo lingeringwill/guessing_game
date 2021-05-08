cf7448d127ffc5a796e36a18d2f958aa6ec024f6
joshua <blitzmasterj24@gmail.com>
2021-05-07 23:32:55 -0400

    first

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    //sets while loop to be active or inactive
+    let count: bool = false;
+}
+
+fn input(){
+
+    println!("please enter in a number");
+    // stores the users guesses
+    let mut guess_container= vec[];
+
+    std::io::stdin().read_line(&mut guess_container)
+    .expect("failed to enter value");
+
+}
